265de9276d66e5e8559eaacd37546352c085f595
Grigoriy Petukhov <lizendir@gmail.com>
2009-01-28 18:19:33 +0600

    tune styles

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'feedzilla'             # Or path to database file if using sqlite3.
+DATABASE_NAME = 'XXX'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'XXX'             # Not used with sqlite3.
 DATABASE_PASSWORD = 'XXX'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.

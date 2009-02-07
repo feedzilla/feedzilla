4b11b1c777b3e3693cf5f7652e0e1780fc734c80
Grigoriy Petukhov <lizendir@gmail.com>
2009-02-07 10:16:31 +0600

    move tags to the bottom of message; make tags the active links

--- feed/urls.py
+++ feed/urls.py
@@ -4,7 +4,7 @@ urlpatterns = patterns('feed.views',
     url(r'^$', 'index', name='index'),
     url('^post/(?P<post_id>\d+)/$', 'post', name='feedzilla_post'),
     url('^feed/(?P<feed_id>\d+)/$', 'feed'),
-    url('^tag/(?P<tag_value>.+)/$', 'tag'),
+    url('^tag/(?P<tag_value>.+)/$', 'tag', name='feedzilla_tag'),
     url('^sources/$', 'sources'),
     url('^search/$', 'search', name='search'),
 )

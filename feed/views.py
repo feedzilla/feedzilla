4ceb932133a77a4679125e0de37092d71a180f68
Grigoriy Petukhov <lizendir@gmail.com>
2009-02-14 22:39:29 +0600

    fix 500 error on missing tag page

--- feed/views.py
+++ feed/views.py
@@ -3,6 +3,7 @@ import re
 
 from django.core.urlresolvers import reverse
 from django.conf import settings
+from django.shortcuts import get_object_or_404
 
 from common.decorators import render_to, paged
 from tagging.models import Tag, TaggedItem
@@ -14,34 +15,15 @@ from common.util import paginate
 def index(request):
     qs = Post.active_objects.all().select_related('feed')
     page, paginator = paginate(qs, request, settings.FEEDZILLA_PAGE_SIZE)
+
     return {'page': page,
             'paginator': paginator,
             }
 
 
-@render_to('feed/post.html')
-def post(request, post_id):
-    post = Post.active_objects.get(pk=post_id)
-    #ft_query = fulltext_search_query(' '.join(re.split(ur'\s+', post.title)[:5]))
-
-    return {'verbose': True,
-            'post': post,
-            } 
-
-
-@render_to('feed/feed.html')
-def feed(request, feed_id):
-    feed = Feed.objects.get(pk=feed_id)
-    last_posts = feed.posts.all().filter(active=True)[:10]
-
-    return {'feed': feed,
-            'last_posts': last_posts,
-            }
-
-
 @render_to('feed/tag.html')
 def tag(request, tag_value):
-    tag = Tag.objects.get(name=tag_value)
+    tag = get_object_or_404(Tag, name=tag_value)
     qs = TaggedItem.objects.get_by_model(Post, tag).filter(active=True).order_by('-created')
     page, paginator = paginate(qs, request, settings.FEEDZILLA_PAGE_SIZE)
 
@@ -53,7 +35,6 @@ def tag(request, tag_value):
 
 @render_to('feed/sources.html')
 def sources(request):
-
     return {'feed': Feed.objects.all(),
             }
 

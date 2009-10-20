aee0113ef021114f18ce19b6a7ee88be44f6f1ba
Grigoriy Petukhov <lorien@lorien.name>
2009-10-20 23:45:10 +0700

    Fixes!

--- feedzilla/context_processors.py
+++ feedzilla/context_processors.py
@@ -6,4 +6,5 @@ def feed(request):
     return {'FEEDZILLA_SITE_TITLE': app_settings.SITE_TITLE,
             'FEEDZILLA_SITE_DESCRIPTION': app_settings.SITE_DESCRIPTION,
             'FEEDZILLA_HEAD_LINKS': app_settings.HEAD_LINKS,
+            'FEEDZILLA_COPYRIGHTS': app_settings.COPYRIGHTS,
             }

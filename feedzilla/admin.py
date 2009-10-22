35ae1ffa1acb08b3a0d7d58090f5a408d371176f
Grigoriy Petukhov <lorien@lorien.name>
2009-10-22 15:15:44 +0700

    Improve admin interface

--- feedzilla/admin.py
+++ feedzilla/admin.py
@@ -4,13 +4,27 @@ from django.contrib import admin
 from feedzilla.models import Feed, Post, FilterTag, FilterWord
 
 class FeedAdmin(admin.ModelAdmin):
-    list_display = ['title', 'feed_url', 'active', 'last_checked']
+    list_display = ['title', 'active', 'last_checked',
+                    'admin_feed_url', 'admin_site_url']
     search_fields = ['title', 'site_url']
 
+    def admin_feed_url(self, obj):
+        return u'<a href="%s">ссылка</a>' % obj.feed_url
+    admin_feed_url.allow_tags = True
+
+    def admin_site_url(self, obj):
+        return u'<a href="%s">ссылка</a>' % obj.site_url
+    admin_site_url.allow_tags = True
+
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'feed', 'created', 'active']
+    list_display = ['title', 'feed', 'created', 'active',
+                    'admin_post_link']
     list_filter = ['feed']
     search_fields = ['title', 'link', 'feed__title']
+
+    def admin_post_link(self, obj):
+        return u'<a href="%s">ссылка</a>' % obj.link
+    admin_post_link.allow_tags = True
 
 class FilterTagAdmin(admin.ModelAdmin):
     list_display = ['value']

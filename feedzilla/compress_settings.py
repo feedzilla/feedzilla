cd000268e87148975916edd7b58ccd46a7ce2083
Grigoriy Petukhov <lorien@lorien.name>
2010-03-27 15:18:05 +0600

    Integrate django-compress

---
+++ feedzilla/compress_settings.py
@@ -0,0 +1,15 @@
+COMPRESS_CSS = {
+    'common_styles': {
+        'source_filenames': ('feedzilla/css/reset.css', 'feedzilla/css/style.css'),
+        'output_filename': 'css/common_styles.r?.css',
+    }
+}
+
+COMPRESS_JS = {
+    'common_scripts': {
+        'source_filenames': ('feedzilla/js/jquery.js', 'feedzilla/js/fix_tags.js',
+                             'feedzilla/js/jquery.highlight-2.js'),
+        'output_filename': 'js/common_styles.r?.js',
+    }
+}
+COMPRESS_VERSION = True

e709947d5cc71152119c8d628d77d253131d9145
Grigoriy Petukhov <lorien@lorien.name>
2010-03-28 17:26:57 +0700

    Refactor context_processor. Add option for displaying twitter button

--- feedzilla/context_processors.py
+++ feedzilla/context_processors.py
@@ -1,10 +1,9 @@
 from django.conf import settings
 
-KEYS = ('FEEDZILLA_SITE_TITLE', 'FEEDZILLA_SITE_DESCRIPTION',
-        'FEEDZILLA_HEAD_LINKS', 'FEEDZILLA_COPYRIGHTS',
-        'FEEDZILLA_FEEDBURNER_FEED', 'FEEDZILLA_FEEDBURNER_COUNTER',
-        'FEEDZILLA_TWITTER')
-CONTEXT = dict((x, getattr(settings, x)) for x in KEYS)
+CONTEXT = {}
+for key in settings.get_all_members():
+    if key.startswith('FEEDZILLA_'):
+        CONTEXT[key] = getattr(settings, key)
 
 def settings(request):
     return CONTEXT

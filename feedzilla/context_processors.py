4b51b61a8b2559e86ef9cca007b7ec9209d46555
Grigoriy Petukhov <lorien@lorien.name>
2011-03-15 01:30:27 +0600

    Remove some stupid things

--- feedzilla/context_processors.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-
-CONTEXT = {}
-for key in dir(settings):
-    if key.startswith('FEEDZILLA_'):
-        CONTEXT[key] = getattr(settings, key)
-
-def settings(request):
-    return CONTEXT
--- feedzilla/settings.py
+++ feedzilla/settings.py
@@ -1,14 +1,4 @@
-from datetime import datetime
-
 FEEDZILLA_PAGE_SIZE = 25
 FEEDZILLA_SUMMARY_SIZE = 2000
 FEEDZILLA_SITE_TITLE = 'Yet another feedzilla site'
 FEEDZILLA_SITE_DESCRIPTION = 'Edit your settings to change that line'
-FEEDZILLA_HEAD_LINKS = ()
-FEEDZILLA_COPYRIGHTS = '%d &copy; Feedzilla Aggregator' % datetime.now().year
-FEEDZILLA_FEEDBURNER_FEED = None
-FEEDZILLA_FEEDBURNER_COUNTER = False
-FEEDZILLA_TWITTER = False
-FEEDZILLA_TWITTER_COUNTER = False
-FEEDZILLA_SUBMIT_SITE_BLOCK = True
-FEEDZILLA_TAG_CLOUD = True

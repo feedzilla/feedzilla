19cc22d14267ff6a3482f9c1f6af36cf1316e0e9
Grigoriy Petukhov <lorien@lorien.name>
2011-07-17 15:23:17 +0700

    Cleanup setup.py and MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,40 +1,15 @@
-import os
-from setuptools import setup
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
-
-PACKAGE = 'feedzilla'
-
-for dirpath, dirnames, filenames in os.walk(PACKAGE):
-    for i, dirname in enumerate(dirnames):
-        if dirname in ['.', '..']:
-            del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath[len(PACKAGE) + 1:] # Strip package directory + path separator
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
+from setuptools import setup, find_packages
 
 setup(
-    version = '0.1.17',
+    name = 'feedzilla',
+    version = '0.1.18',
     description = 'Django application for atom/rss feeds aggregation i.e. planet engine',
+    url = 'http://bitbucket.org/lorien/feedzilla',
     author = 'Grigoriy Petukhov',
     author_email = 'lorien@lorien.name',
-    url = 'http://bitbucket.org/lorien/feedzilla',
-    name = 'feedzilla',
 
-    packages = packages,
-    package_data = {'feedzilla': data_files},
+    packages = find_packages(),
+    include_package_data = True,
 
     license = "BSD",
     keywords = "django application feeds syndication aggregation atom rss planet",
